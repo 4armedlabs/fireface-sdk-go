e2f6fd30c38a2c2de7e7c1ec5d19385c911ad4b4
Marc Wickenden <marc@4armed.com>
2024-07-03 11:40:22 +0100

    more work on client

--- auth/auth.go
+++ auth/auth.go
@@ -2,12 +2,69 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"net/http"
+	"time"
+
+	"github.com/lestrrat-go/jwx/v2/jwk"
 )
 
-type Client struct {
+type client struct {
 	secretKey  string
 	httpClient *http.Client
+	baseURL    string
+	jwksURL    string
+	jwksCache  *jwk.Cache
+}
+
+type Client struct {
+	*client
+}
+
+type AuthClientOption func(*client)
+
+type AuthConfig struct {
+	BaseURL   string
+	SecretKey string
+	Opts      []AuthClientOption
+	Version   string
+}
+
+func NewClient(ctx context.Context, config *AuthConfig) (*Client, error) {
+	baseClient := &client{}
+
+	if config == nil {
+		return nil, errors.New("config is nil")
+	}
+
+	if config.SecretKey == "" {
+		return nil, errors.New("secret key is required")
+	}
+
+	baseClient.secretKey = config.SecretKey
+	baseClient.baseURL = config.BaseURL
+	baseClient.jwksURL = config.BaseURL + "/.well-known/jwks.json"
+
+	baseClient.httpClient = newHTTPClient()
+
+	jwksCache := jwk.NewCache(ctx)
+	jwksCache.Register(baseClient.jwksURL, jwk.WithMinRefreshInterval(5*time.Minute))
+
+	// Refresh the cache immediately to populate
+	_, err := jwksCache.Refresh(ctx, baseClient.jwksURL)
+	if err != nil {
+		return nil, err
+	}
+
+	baseClient.jwksCache = jwksCache
+
+	for _, opt := range config.Opts {
+		opt(baseClient)
+	}
+
+	return &Client{
+		client: baseClient,
+	}, nil
 }
 
 type DecodedIdToken struct {
@@ -21,4 +78,16 @@ type DecodedIdToken struct {
 
 func (c *Client) VerifyIDToken(ctx context.Context, idToken string) (DecodedIdToken, error) {
 	return DecodedIdToken{}, nil
+}
+
+type HttpClientOption func(*http.Client)
+
+func newHTTPClient(opts ...HttpClientOption) *http.Client {
+	hc := &http.Client{}
+
+	for _, opt := range opts {
+		opt(hc)
+	}
+
+	return hc
 }
